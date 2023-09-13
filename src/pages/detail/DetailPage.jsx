07c84863237677e1da396b0a60a8a35c13729d28
Giao Vu Dinh <tvudinh20@gmail.com>
2023-09-13 01:13:24 -0400

    detailspages

--- src/pages/detail/DetailPage.jsx
+++ src/pages/detail/DetailPage.jsx
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+import { items as codeItems } from "../code/codeItems";
+import { items as artItems } from "../art/artItems";
+
+import "./detailpage.css";
+
 function Detail(props) {
+  const [item, setItem] = useState();
   const { type } = props;
   const { projectId } = useParams();
+
+  useEffect(() => {
+    if (type === "code") {
+      const res = codeItems.find((c) => c.id === projectId);
+      setItem(res);
+    } else {
+      const res = artItems.find((a) => a.id === projectId);
+      setItem(res);
+    }
+  }, [projectId, type]);
+
+  const renderImages = () => {
+    return item?.images.map((image, i) => {
+      return <img key={i} src={image} alt={item?.title} />;
+    });
+  };
+
   return (
-    <div className="textbox">
-      Detail page: {type} {projectId}
+    <div className="twocolumns">
+      <div className="detail__column detail__column--left">
+        <h3 style={{ marginBottom: "20px" }}>{item?.title}</h3>
+        <h4>
+          <b>{type === "code" ? "timeline" : "Date"}:</b> {item?.date}
+        </h4>
+        <h4>
+          <b>{type === "code" ? "role" : "type"}:</b>{" "}
+          {type === "code" ? item?.role : item?.type}
+        </h4>
+        <h4>
+          <b>{type === "code" ? "stack" : "software"}:</b>{" "}
+          {type === "code" ? item?.stack : item?.software}
+        </h4>
+        <h4 style={{ marginBottom: "35px" }}>
+          <b>purpose: </b>
+          {item?.purpose}
+        </h4>
+        <p dangerouslySetInnerHTML={{ __html: item?.description }} />
+        {/* <p>{item?.description}</p> */}
+      </div>
+      <div className="detail__column detail__column--right">
+        {renderImages()}
+      </div>
     </div>
   );
 }
