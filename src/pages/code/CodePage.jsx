b322104844d6bc266f1ee7796e001bf5a2571808
Giao Vu Dinh <tvudinh20@gmail.com>
2023-09-12 21:40:07 -0400

    code page

--- src/pages/code/CodePage.jsx
+++ src/pages/code/CodePage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Gallery from "../../components/Gallery";
+import { items } from "./codeItems";
 
 function Code() {
-  return <div className="textbox">Code</div>;
+  return (
+    <>
+      <div className="headerbox vertical-align">
+        <h3>Code</h3>
+        <p>a selection of projects that I've worked on. </p>
+      </div>
+      <Gallery items={items} />
+      <div className="spacer"></div>
+    </>
+  );
 }
 
 export default Code;
